d60354eb09095844753110ea35d0e099862365e4
pote26 <pote.41b2@gmail.com>
2023-10-30 01:09:18 +0900

    main commit
    main commit

---
+++ SampleProject/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  SampleProject
+//
+//  Created by pote26 on 30/10/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello Swift!")
+        view.backgroundColor = .green
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
