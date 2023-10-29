b55fbaec49102d4b1eafc4c48a72513c39413190
pote26 <pote.41b2@gmail.com>
2023-10-30 00:57:32 +0900

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by pote26 on 30/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
